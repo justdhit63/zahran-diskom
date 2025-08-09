df7f2f3f2c5d4786b290f5fac613d71d25ba8177
justdhit63 <muhadhit0603@gmail.com>
2025-08-09 19:14:53 +0700

    layouting

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+
+    <div class="pt-20 pb-12">
+        <h1 class="text-3xl font-medium ml-8">Dashboard</h1>
+        <h4 class="ml-8 mb-8"><span class="text-gray-600">Home / </span>Dashboard</h4>
+        <div class="w-full grid grid-cols-2 gap-8 items-center mx-auto sm:px-6 lg:px-8 mb-8">
+            <div class="w-full bg-white overflow-hidden shadow-md border border-gray-200 sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+            <div class="w-full bg-white overflow-hidden shadow-md border border-gray-200 sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+        <div class="px-8 mb-8">
+            <div class="w-full bg-white overflow-hidden shadow-md border border-gray-200 sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+        <div class="px-8 mb-8 h-screen">
+            <div class="w-full bg-white overflow-hidden shadow-md border border-gray-200 sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="w-full text-center mx-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, obcaecati?</footer>
+</x-app-layout>
